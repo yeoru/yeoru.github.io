ce3970232ddaf71834019db3ef304d359c644c0b
YeongJu LoRa Yang <yyj4561@gmail.com>
2024-08-12 01:17:48 +0900

    a

--- express.js
+++ express.js
@@ -5,12 +5,11 @@ const port = 3000;
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("./path/to/serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-
 
 app.use(express.json());
 
